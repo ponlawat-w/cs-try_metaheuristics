e27b1136969ce6e584f105ac09d74dda3b166989
ponlawat-w <ponlawat_w@cmu.ac.th>
2018-10-26 15:52:42 +0700

    Add project files.

---
+++ TryMetaHeuristics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryMetaHeuristics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
